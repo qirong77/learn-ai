21540359725da7e6e353a62630ab4432784701c5
qirong77 <qirong7777@gmail.com>
2026-01-04 21:17:53 +0800

    feat: 功能开发 ✨

---
+++ examples/playwright-ai/PROJECT_PLAN.md
@@ -0,0 +1,2 @@
+这个项目是基于 ai + playwright + langchain 的浏览器自动玩原神。
+请你先进行项目的规划。输出在一个 markdown 文件汇总，不需要写代码，
--- examples/playwright-ai/agent.ts
+++ examples/playwright-ai/agent.ts
@@ -1,3 +1,6 @@
 import { chromium } from "playwright";
-const task = "前往云-原神网页，https://ys.mihoyo.com/cloud/#/home，登录账号，进入抽卡页面，进行常驻祈愿抽卡 2 次。";
+const task_example_1 = "前往云-原神网页，https://ys.mihoyo.com/cloud/#/home，登录账号，进入抽卡页面，进行常驻祈愿抽卡 2 次。";
+const task_example_2 = "进行秘境挑战，随机选择一个秘境进行挑战，完成后返回蒙德城传送点。";
+const task_example_3 = "前往蒙德城，找到酒馆老板娘凯瑟琳，和她对话，接受她的任务，然后前往风起地完成任务。";
+
 
